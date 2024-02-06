d49277df013a82f78cdcdcf7ccc5714e42b52151
Saskia Hiltemann <zazkia@gmail.com>
2024-02-06 13:24:16 +0100

    fix page title

--- resources/views/domain/index.blade.php
+++ resources/views/domain/index.blade.php
@@ -6,7 +6,7 @@
 
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
-  <h1 class="display-6">Species Table</h1>
+  <h1 class="display-6">Domains Table</h1>
     <p class="lead">This is a table of all domains covered in TAPscan, including custom domains. </p>
     <hr class="my-1">
     </p>
