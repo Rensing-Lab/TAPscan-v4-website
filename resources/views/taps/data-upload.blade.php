939af98bce8b1b0ad320e6fdd719026bc24d78cc
Saskia Hiltemann <zazkia@gmail.com>
2024-02-06 11:34:00 +0100

    add information about file formats to Admin Upload Interface

--- resources/views/taps/data-upload.blade.php
+++ resources/views/taps/data-upload.blade.php
@@ -41,11 +41,48 @@
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-
           <form id="import_{{$button}}_modal" action="{{ route($button.'.import') }}" method="POST" enctype="multipart/form-data">
               @csrf
           <div class="modal-body">
+
             <input type="file" name="file" id="{{$button}}_file" class="form-control" multiple/>
+          @if ($button == "species")
+          <p><ul>
+             <li>Must a semicolon (<code>;</code>) separated file without a header line </li>
+             <li>With 8 columns: <code>lettercode;Kingdom/supergroup;phylum/clade; supergroup2;order;family;scientific name;NCBI TaxID</code></li>
+             <li><a target="_blank" href="https://github.com/Rensing-Lab/TAPscan-v4-website/blob/main/_data/import-species/species_v4.csv">Example species file</a></li>
+             </ul>
+          </p>
+          @elseif ($button == "rules")
+          <p><ul>
+             <li>Must be a semicolon (<code>;</code>) separated file without a header line </li>
+             <li>With 3 columns: <code>TAP family;Domain;Rule (should | should not)</code></li>
+             <li><a target="_blank" href="https://github.com/Rensing-Lab/TAPscan-v4-website/blob/main/_data/import-rules/rules_v81.txt">Example rules file</a></li>
+             </ul>
+           </p>
+          @elseif ($button == "tap")
+          <p><ul>
+             <li>Provide the subfamily classification output (<code>*.output.3</code>) from the TAPscan-classify script</li>
+             <li>Must be a semicolon (<code>;</code>) separate file, with a header line </li>
+             <li>With 5+ columns: <code>GENE_ID;TAP_FAMILY;SUBFAMILY;NUMBER_OF_FAMILIES_FOUND;DOMAIN1;DOMAIN2;..</code></li>
+             <li><a target="_blank" href="https://github.com/Rensing-Lab/TAPscan-v4-website/blob/main/_data/import-tap/taps_v4.csv">Example TAPs file</a></li>
+             </ul>
+          </p>
+          @elseif ($button == "tapinfo")
+          <p><ul>
+             <li>Must be a tab-separated file, with a header line</li>
+             <li>With 4 columns: <code>TAP name [TAB] Description [TAB] Reference1,reference2,.. [TAB] Type (TR|TF|PT) </code></li>
+             <li><a target="_blank" href="https://github.com/Rensing-Lab/TAPscan-v4-website/blob/main/_data/import-tapinfo/tapinfo_v4.csv">Example TapInfo File</a></li>
+             </ul>
+          </p>
+          @elseif ($button == "domains")
+          <p><ul>
+             <li>Must be a semicolon (<code>;</code>) separated file without a header line </li>
+             <li>with 2 columns: <code> Domain; PFAM ID</code></li>
+             <li><a target="_blank" href="https://github.com/Rensing-Lab/TAPscan-v4-website/blob/main/_data/import-domain/domains_v4.csv">Example Domains File</a></li>
+             </ul>
+          </p>
+          @endif
           </div>
           <div class="modal-footer">
             <button type="submit" class="btn btn-primary">Import</button>
