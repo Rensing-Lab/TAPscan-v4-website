cf2381ba975047c85aa88152d098215e1ef9e5d7
Saskia Hiltemann <zazkia@gmail.com>
2024-05-27 14:53:31 +0200

    fix dates of news items. Add reference to About page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,24 +38,26 @@ TAPscan and [PLAZA](https://bioinformatics.psb.ugent.be/plaza/) now cross link t
 TAPscan was published December 2017, please cite: <br>
 **TAPscan - Comprehensive genome-wide classification reveals that many plant-specific transcription factors evolved in streptophyte algae. Wilhelmsson P.K., Mühlich C., Ullrich K.K., Rensing S.A. (2017) [Genome Biology and Evolution, evx258](https://academic.oup.com/gbe/advance-article/doi/10.1093/gbe/evx258/4693820?guestAccessKey=fc717224-f2bb-452b-91a2-982f5fbd7489)**
 ",
-            'created_at' => date("Y-m-d H:i:s") ,
-            'updated_at' => date("Y-m-d H:i:s") ,
+            'created_at' => date("Y-m-d H:i:s", mktime(12, 42, 0, 5, 24, 2024))  ,
+            'updated_at' => date("Y-m-d H:i:s", mktime(12, 42, 0, 5, 24, 2024))  ,
 
         ]);
 
         DB::table('news')->insert([
             'name' => "TAPscan v4 is out!",
             'content' => '
-We are happy to announce the new version 4 of TAPscan. This comprehensive update includes the addition of 23 new (sub) families, allowing TAPscan v4 to detect 145 TAP families. This update also includes an updated web interface, which enables to interactively explore the TAP data. By incorporating the <a target="_blank" href="https://github.com/Rensing-Lab/Genome-Zoo"> Genomezoo dataset</a>, in addition to TAP data of the Archaeplastida, now also data of selected fungi, mammals, SAR group species, bacteria and archaea is made available throughout the TAPscan web interface.
+We are happy to announce the new version 4 of TAPscan. This comprehensive update includes the addition of 18 new (sub) families, allowing TAPscan v4 to detect 138 TAP families. This update also includes an updated web interface, which enables to interactively explore the TAP data. By incorporating the <a target="_blank" href="https://github.com/Rensing-Lab/Genome-Zoo"> Genome Zoo dataset</a>, in addition to TAP data of the Archaeplastida, now also data of selected fungi, mammals, SAR group species, bacteria and archaea is made available throughout the TAPscan web interface.
 <br><br>
 TAPscan v4 has also been made available as a Galaxy tool as part of the European Galaxy server (<a target="_blank" href="https://usegalaxy.eu">usegalaxy.eu</a>).
 <br><br>
 In addition, the TAP information derived from the recently published (updated) genomes of Takakia lepidozioides (Hu et al., 2023), Mesotaenium endlicherianum  (Dadras et al., 2023) and Zygnema circumcarinatum (Feng et al., 2023) is made available.
 <br><br>
-More information about the update can be found within the TAPscan v4 publication in The Plant Journal (in submission)!
+More information about the update will soon be available in the  TAPscan v4 publication.
+
+See the <a href="/about">About page</a> for more information including version history and source code and data availability.
 ',
-            'created_at' => date("Y-m-d H:i:s") ,
-            'updated_at' => date("Y-m-d H:i:s") ,
+            'created_at' => date("Y-m-d H:i:s", mktime(12, 42, 0, 5, 24, 2024)) ,
+            'updated_at' => date("Y-m-d H:i:s", mktime(12, 42, 0, 5, 24, 2024)) ,
 
         ]);
     }
